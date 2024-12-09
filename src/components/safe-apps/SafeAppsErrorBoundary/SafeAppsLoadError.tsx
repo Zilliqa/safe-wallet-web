6d660a06951245d00e641941434d95bd05712cab
ajimeno04 <jimenobeltran11@gmail.com>
2024-12-09 14:38:01 +0100

    feat: update safeappsLoad error white space

--- src/components/safe-apps/SafeAppsErrorBoundary/SafeAppsLoadError.tsx
+++ src/components/safe-apps/SafeAppsErrorBoundary/SafeAppsLoadError.tsx
@@ -20,8 +20,9 @@ const SafeAppsLoadError = ({ onBackToApps }: SafeAppsLoadErrorProps): React.Reac
         <SvgIcon component={NetworkError} inheritViewBox className={css.image} />
 
         <div>
-          <Typography component="span">In case the problem persists, please report it to </Typography>{' '}
+          <Typography component="span">In case the problem persists, please report it through our</Typography>
           <ExternalLink href={'https://safe-support.protofire.io/'} fontSize="medium">
+            {''}
             Form
           </ExternalLink>
         </div>
