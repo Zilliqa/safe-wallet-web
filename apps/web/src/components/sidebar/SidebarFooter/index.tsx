9ae2f34d9fd617d43807e1fa7bc90ea0fcd25e61
ajimeno04 <jimenobeltran11@gmail.com>
2025-07-15 15:03:58 +0200

    chore: Refactor SidebarFooter component imports and add new constant for suggestion form

--- apps/web/src/components/sidebar/SidebarFooter/index.tsx
+++ apps/web/src/components/sidebar/SidebarFooter/index.tsx
@@ -7,22 +7,21 @@ import {
   SidebarListItemIcon,
   SidebarListItemText,
 } from '@/components/sidebar/SidebarList'
-import { /* BEAMER_SELECTOR, */ loadBeamer } from '@/services/beamer'
+import { /* BEAMER_SELECTOR, */ BEAMER_SELECTOR, loadBeamer } from '@/services/beamer'
 import { useAppSelector } from '@/store'
 import { CookieAndTermType, hasConsentFor } from '@/store/cookiesAndTermsSlice'
-//import { openCookieBanner } from '@/store/popupSlice'
 //import BeamerIcon from '@/public/images/sidebar/whats-new.svg'
 import HelpCenterIcon from '@/public/images/sidebar/help-center.svg'
+//import LightIcon from '@/public/images/sidebar/light-icon.png'
+import ProtofireLogo from '@/public/images/protofire-logo.svg'
 import { Link, ListItem, SvgIcon, Typography } from '@mui/material'
 import DebugToggle from '../DebugToggle'
-import { HELP_CENTER_URL, IS_PRODUCTION } from '@/config/constants'
+import { HELP_CENTER_URL, IS_PRODUCTION, NEW_SUGGESTION_FORM } from '@/config/constants'
 import Track from '@/components/common/Track'
 import { OVERVIEW_EVENTS } from '@/services/analytics/events/overview'
 import { useCurrentChain } from '@/hooks/useChains'
 import darkPalette from '@/components/theme/darkPalette'
 import SuggestionIcon from '@/public/images/sidebar/lightbulb_icon.svg'
-import ProtofireLogo from '@/public/images/protofire-logo.svg'
-
 const SidebarFooter = (): ReactElement => {
   //const dispatch = useAppDispatch()
   const chain = useCurrentChain()
