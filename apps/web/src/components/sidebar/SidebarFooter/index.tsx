11209de04fc6a4e98e7b9c1f491c1937904ec401
den <dzianis.smalonski@protofire.io>
2025-08-22 17:55:47 +0200

    refactor: update SVG logo size and clean up sidebar footer links

--- apps/web/src/components/sidebar/SidebarFooter/index.tsx
+++ apps/web/src/components/sidebar/SidebarFooter/index.tsx
@@ -11,8 +11,6 @@ import HelpCenterIcon from '@/public/images/sidebar/help-center.svg'
 import { Divider, Link, ListItem, SvgIcon, Typography } from '@mui/material'
 import DebugToggle from '../DebugToggle'
 import { HELP_CENTER_URL, IS_PRODUCTION } from '@/config/constants'
-import Track from '@/components/common/Track'
-import { OVERVIEW_EVENTS } from '@/services/analytics/events/overview'
 import { useCurrentChain } from '@/hooks/useChains'
 import darkPalette from '@/components/theme/darkPalette'
 //import SuggestionIcon from '@/public/images/sidebar/lightbulb_icon.svg'
@@ -61,36 +59,18 @@ const SidebarFooter = (): ReactElement => {
         </ListItem>
       </Track> */}
 
-      <Track {...OVERVIEW_EVENTS.HELP_CENTER}>
-        <ListItem disablePadding>
-          <a target="_blank" rel="noopener noreferrer" href={HELP_CENTER_URL} style={{ width: '100%' }}>
-            <SidebarListItemButton>
-              <SidebarListItemIcon color="primary">
-                <HelpCenterIcon />
-              </SidebarListItemIcon>
-              <SidebarListItemText data-testid="list-item-need-help" bold>
-                Need help?
-              </SidebarListItemText>
-            </SidebarListItemButton>
-          </a>
-        </ListItem>
-      </Track>
-      {/* <Track {...OVERVIEW_EVENTS.SUGGESTIONS}>
-        <ListItem disablePadding>
-          <a target="_blank" rel="noopener noreferrer" href={NEW_SUGGESTION_FORM} style={{ width: '100%' }}>
-            <SidebarListItemButton
-              id={BEAMER_SELECTOR}
-              style={{ backgroundColor: '#12FF80', color: 'black' }}
-              onClick={handleBeamer}
-            >
-              <SidebarListItemIcon color="primary">
-                <SuggestionIcon />
-              </SidebarListItemIcon>
-              <SidebarListItemText bold>New Features Suggestion?</SidebarListItemText>
-            </SidebarListItemButton>
-          </a>
-        </ListItem>
-      </Track> */}
+      <ListItem style={{ padding: 'var(--space-1)' }}>
+        <a target="_blank" rel="noopener noreferrer" href={HELP_CENTER_URL} style={{ width: '100%' }}>
+          <SidebarListItemButton>
+            <SidebarListItemIcon color="primary">
+              <HelpCenterIcon />
+            </SidebarListItemIcon>
+            <SidebarListItemText data-testid="list-item-need-help" bold>
+              Need help?
+            </SidebarListItemText>
+          </SidebarListItemButton>
+        </a>
+      </ListItem>
       <ListItem>
         <SidebarListItemText>
           <Typography variant="caption">
